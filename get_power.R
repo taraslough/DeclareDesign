615f56c42adfb43ec88cb18bde3a949526221517
Alexander Coppock <acoppock@gmail.com>
2015-05-24 22:06:47 -0600

    get_power skeleton

---
+++ get_power.R
@@ -0,0 +1,14 @@
+
+
+get_power <- function(data, design, test_stat, sims=1000){
+  
+}
+
+# series of test statistics
+
+test_coefficients <- function(formula, which_coefs, data){
+  
+}
+  
+  
+  
