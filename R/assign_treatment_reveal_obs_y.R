90724c38f141c771408f792d3da9f52c652a2e0b
graemeblair <graeme.blair@gmail.com>
2015-06-17 13:15:37 -0400

    rename package; fixed NA issue in reveal_observed_outcome

--- R/assign_treatment_reveal_obs_y.R
+++ R/assign_treatment_reveal_obs_y.R
@@ -1,19 +1,18 @@
-
-
 #' Assign treatment status
 #'
 #' Description
 #' @param design A design object created by declare_design; or a function that assigns treatment
+#' @param ... Options to override defaults in random assignmetn function
 #' @return a random assignment vector
 #' @examples
 #' # some examples go here
 #' @export
-assign_treatment <- function(design) {
+assign_treatment <- function(design, ...) {
   
   ## should be expanded to take either a design object or a function
   ## may be need to have this respond to the characteristics of the data frame, i.e. N and m?
 
-  return(design())
+  return(design(...))
   
 }
 
@@ -30,7 +29,7 @@ assign_treatment <- function(design) {
 observed_outcome <- function(outcome, treatment_assignment, data){
   
   if(any(is.na(data[,treatment_assignment]))>0)
-    stop("There are NA's in the treatment assignment vector.")
+    warning("There are NA's in the treatment assignment vector.")
 
   observed_y <- rep(NA, nrow(data))
   for(v in treat_vals){
