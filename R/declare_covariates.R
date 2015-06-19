89a807b92078db7499f90e6528945035d457306d
jaspercooper <jaspercooper@gmail.com>
2015-06-19 15:50:11 +0300

    Made declare_covariates()
    Currently can create matrices with no levels (i.e. you just input the
    variables you want), or N-level matrices that are created through lists
    of variables corresponding to successive nested levels. Needs some work.

---
+++ R/declare_covariates.R
@@ -0,0 +1,142 @@
+#' Declare the structure of the covariates 
+#'
+#' @param ... a list either of variable declarations, or of lists of variable declarations (one per level)
+#' @export
+declare_covariates <-
+  function(...,N_per_level=NULL,lower_units_per_level=NULL) {
+    
+    make_X_matrix <-
+      function(variables,variable_names,N){
+        fun.list <- lapply(variables,function(variable) {
+          
+          if (!class(variable) %in% c("function","DGP_object")) {
+            stop(
+              "Variables should either be random number functions or DGP_object (see declare_variable())"
+            )
+          }
+          if (class(variable) == "function") {
+            return(variable)
+          }
+          if (variable$distribution == "normal") {
+            return(function(){rnorm(n=N,mean = variable$mean, sd = variable$sd)})
+          }
+          if (variable$distribution == "binary") {
+            return(function(){binom_out <- rbinom(n = N,size = 1,prob =variable$probability)
+            if(!is.null(variable$categories)){binom_out <- 
+              factor(binom_out,c(0,1),
+                     variable$categories)}
+            return(binom_out)})
+          }
+          if (variable$distribution == "multinomial") {
+            return(function(){multinom_out <- 
+              apply(rmultinom(n = N,size = 1,
+                              prob =variable$probability),2,function(i)which(i==1))
+            if(!is.null(variable$categories)){
+              multinom_out <- factor(multinom_out,
+                                     levels = 1:length(variable$categories),
+                                     labels = variable$categories)}
+            return(multinom_out)})
+          }
+        })
+        x <- lapply(fun.list,function(f)f())
+        X.char <- do.call(cbind.data.frame,x)
+        X <- data.frame(matrix(rep(NA,dim(X.char)[1]*dim(X.char)[2]),nrow = dim(X.char)[1]))
+        for(i in 1:dim(X.char)[2]){
+          X[,i] <- X.char[i][,1]
+        }
+        names(X) <- variable_names
+        return(X)
+      }
+    
+    # Put in checks here to check how the list is structured
+    # i.e. if there are variable declarations or functions in the top level it should
+    # only be those, no lists
+    # and if there are levels, then the levels should be named and have
+    
+    variable_list  <- list(...)
+    
+    if(TRUE %in% (c("function","DGP_object")%in%sapply(variable_list,class))){
+      N_levels <- 1
+      make_covariates <- function(){make_X_matrix(variables = variable_list,
+                                                  variable_names = names(variable_list),
+                                                  N = N_units
+      )} 
+      level_names <- "Only one level"
+      # data_structure_description <- "Only one level"
+      variable_names <- names(variable_list)
+      
+    }else{
+      
+      N_levels       <- length(variable_list)
+      level_names    <- names(variable_list)
+      variable_names <- lapply(variable_list, names)
+      
+      # Make object here stating number of levels, which 
+      # looks at each variable_list and tries to figure it out:
+      if(is.null(N_per_level)){
+        # N_per_level    <- lapply(1:N_levels,level_names)
+      }
+      
+      if(is.null(lower_units_per_level)){
+        # lower_units_per_level    <- Some division
+      }
+      
+      make_covariates <- function(){
+        X_list <- lapply(1:N_levels,function(i){
+          X_mat <- make_X_matrix(variables = variable_list[[i]],
+                                 variable_names = variable_names[[i]],
+                                 N = N_per_level[i])
+          X_mat$id <- 1:dim(X_mat)[1]
+          names(X_mat)[names(X_mat)=="id"] <- paste0(level_names[i],"_id")
+          return(X_mat)
+        })
+        
+        id_vars <- sapply(X_list,function(X_to_grab){
+          X_names <- names(X_to_grab)
+          return(X_names[length(X_names)])
+        })
+        
+        X_mat_joined <- NA
+        
+        for(i in N_levels:2){
+          
+          X_list[[i-1]]$merge_id <- 
+            sample(rep(X_list[[i]][,id_vars[i]],
+                       lower_units_per_level[[i]]))
+          
+          names(X_list[[i-1]])[names(X_list[[i-1]])=="merge_id"] <- id_vars[i]
+          
+          X_mat_joined <- 
+            merge(x  = X_list[[i-1]],
+                  y  = X_list[[i]],
+                  by = id_vars[i])
+          
+        }
+      covariate_matrix <- X_mat_joined
+      return(covariate_matrix)
+    }
+    
+    
+#     data_structure_description <- paste0(paste(sapply(2:N_levels,function(i){
+#       if(nested_levels[i-1]){
+#         paste0(level_names[i-1], " is / are nested within ",level_names[i])
+#       }else{paste0(level_names[i-1], 
+#                    " is / are not nested within ",
+#                    level_names[i])}
+#       }),collapse = ", "),".")
+    
+    }
+    covariate_object <- list(
+      make_covariates = make_covariates,
+      covariate_names = variable_names,
+      level_names = level_names,
+      number_levels = N_levels,
+      N_per_level = N_per_level,
+      lower_units_per_level = lower_units_per_level,
+      # data_structure_description = data_structure_description,
+      call = match.call()
+    )
+    class(covariate_object) <- "covariate_object"
+    return(covariate_object)
+  }
+
