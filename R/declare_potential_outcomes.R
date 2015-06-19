66101889dc2ddec1fbef6b4d0ee67bb2f378dbc9
jaspercooper <jaspercooper@gmail.com>
2015-06-19 15:50:44 +0300

    Made declare_potential_outcomes()
    Provide some key info about potential outcomes. Should be integrated
    with get_power()

---
+++ R/declare_potential_outcomes.R
@@ -0,0 +1,27 @@
+#' Define the potential outcomes of the experiment
+#'
+#' @param covariate_object covariates data frame
+#' @return outcomes_object
+#' @export
+declare_potential_outcomes <- 
+  function(
+    condition_names,
+    outcome_formula,
+    outcome_variable_DGP = declare_variable(),
+    cluster_variable = NULL,
+    ICC = NULL
+  ){
+      if(FALSE %in% (condition_names %in% all.vars(outcome_formula)))stop(
+        "All conditions must be included as variables in the outcome formula."
+      )
+      outcomes_object <- list(
+        condition_names = condition_names,
+        outcome_formula = outcome_formula,
+        outcome_variable = outcome_variable_DGP,
+        cluster_variable = cluster_variable,
+        ICC = .01,
+        call = match.call()
+      )
+      class(outcomes_object) <- "outcomes_object"
+      return(outcomes_object)
+    }
