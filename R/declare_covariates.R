b1873d9b5489313c66a26d34bccbda7ea5196b6e
jaspercooper <jaspercooper@gmail.com>
2015-06-22 10:15:29 +0300

    Made internal function make_X_matrix() more flexible and available externally

--- R/declare_covariates.R
+++ R/declare_covariates.R
@@ -5,49 +5,6 @@
 declare_covariates <-
   function(...,N_per_level=NULL,lower_units_per_level=NULL) {
     
-    make_X_matrix <-
-      function(variables,variable_names,N){
-        fun.list <- lapply(variables,function(variable) {
-          
-          if (!class(variable) %in% c("function","DGP_object")) {
-            stop(
-              "Variables should either be random number functions or DGP_object (see declare_variable())"
-            )
-          }
-          if (class(variable) == "function") {
-            return(variable)
-          }
-          if (variable$distribution == "normal") {
-            return(function(){rnorm(n=N,mean = variable$mean, sd = variable$sd)})
-          }
-          if (variable$distribution == "binary") {
-            return(function(){binom_out <- rbinom(n = N,size = 1,prob =variable$probability)
-            if(!is.null(variable$categories)){binom_out <- 
-              factor(binom_out,c(0,1),
-                     variable$categories)}
-            return(binom_out)})
-          }
-          if (variable$distribution == "multinomial") {
-            return(function(){multinom_out <- 
-              apply(rmultinom(n = N,size = 1,
-                              prob =variable$probability),2,function(i)which(i==1))
-            if(!is.null(variable$categories)){
-              multinom_out <- factor(multinom_out,
-                                     levels = 1:length(variable$categories),
-                                     labels = variable$categories)}
-            return(multinom_out)})
-          }
-        })
-        x <- lapply(fun.list,function(f)f())
-        X.char <- do.call(cbind.data.frame,x)
-        X <- data.frame(matrix(rep(NA,dim(X.char)[1]*dim(X.char)[2]),nrow = dim(X.char)[1]))
-        for(i in 1:dim(X.char)[2]){
-          X[,i] <- X.char[i][,1]
-        }
-        names(X) <- variable_names
-        return(X)
-      }
-    
     # Put in checks here to check how the list is structured
     # i.e. if there are variable declarations or functions in the top level it should
     # only be those, no lists
@@ -144,4 +101,64 @@ declare_covariates <-
     class(covariate_object) <- "covariate_object"
     return(covariate_object)
   }
+
+
+#' @export 
+make_X_matrix <-
+  function(...,variable_names = NULL,N){
+    
+    variables  <- list(...)
+    
+    class_list <- sapply(variables,class)
+    if(all(c("function","list")%in%class_list)|
+       all(c("DGP_object","list")%in%class_list))stop(
+         "Function takes either direct variable declarations or a list of variable declarations, not both at once.")
+    if("list"%in%class_list){
+      variables <- unlist(variables,recursive = F)
+    }
+    
+    names_list <- names(variables)
+    
+    fun.list <- lapply(variables,function(variable) {
+      
+      if (!class(variable) %in% c("function","DGP_object")) {
+        stop(
+          "Variables should either be random number functions or DGP_object (see declare_variable())"
+        )
+      }
+      if (class(variable) == "function") {
+        return(variable)
+      }
+      if (variable$distribution == "normal") {
+        return(function(){rnorm(n=N,mean = variable$mean, sd = variable$sd)})
+      }
+      if (variable$distribution == "binary") {
+        return(function(){binom_out <- rbinom(n = N,size = 1,prob =variable$probability)
+        if(!is.null(variable$categories)){binom_out <- 
+          factor(binom_out,c(0,1),
+                 variable$categories)}
+        return(binom_out)})
+      }
+      if (variable$distribution == "multinomial") {
+        return(function(){multinom_out <- 
+          apply(rmultinom(n = N,size = 1,
+                          prob =variable$probability),2,function(i)which(i==1))
+        if(!is.null(variable$categories)){
+          multinom_out <- factor(multinom_out,
+                                 levels = 1:length(variable$categories),
+                                 labels = variable$categories)}
+        return(multinom_out)})
+      }
+    })
+    x <- lapply(fun.list,function(f)f())
+    X.char <- do.call(cbind.data.frame,x)
+    X <- data.frame(matrix(rep(NA,dim(X.char)[1]*dim(X.char)[2]),nrow = dim(X.char)[1]))
+    for(i in 1:dim(X.char)[2]){
+      X[,i] <- X.char[i][,1]
+    }
+    if(!is.null(variable_names))names(X) <- variable_names
+    if(is.null(variable_names))names(X) <- names_list
+    return(X)
+  }
+
 
