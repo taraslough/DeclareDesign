a04dd9939ba735923e7b1a8e7692a9e2f78e5a6d
jaspercooper <jaspercooper@gmail.com>
2015-07-23 21:28:08 -0400

    Added "transformation" option

--- R/declare_variable.R
+++ R/declare_variable.R
@@ -2,38 +2,38 @@
 #'
 #' @param linear_mean If the variable is linear 
 #' @export
-declare_variable <- function(
-  linear_mean        = NULL,
-  linear_sd          = NULL,
-  binary_probability = NULL,
-  binary_categories = NULL,
-  multinomial_probabilities = NULL,
-  multinomial_categories = NULL
-){
-    if(sum(!unlist(lapply(
-      X = list(
-        normal = list(linear_mean,linear_sd),
-        binary = list(binary_probability,binary_categories),
-        multinomial = list(multinomial_probabilities,multinomial_categories)),
-      FUN = function(i)sum(unlist(lapply(i,is.null))))) %in% 2) > 1){stop(
-        "Please enter parameters for only one kind of variable (linear, binary or multinomial).")}
-    if(!is.null(linear_mean)){
-      if(!is.null(linear_sd)){
-        variable <- list(
-          distribution = "normal",
-          mean = linear_mean,
-          sd = linear_sd
-        )
-      }else{
-      variable <- list(
-        distribution = "normal",
-        mean = linear_mean,
-        sd = linear_mean*.10+1
+declare_variable <-
+  function(linear_mean = NULL,linear_sd = NULL,binary_probability = NULL, binary_categories = NULL, multinomial_probabilities = NULL, multinomial_categories = NULL, transformation = NULL) {
+    
+    type_check <- list(
+      normal = c(linear_mean,linear_sd),
+      binary = c(binary_probability,binary_categories),
+      multinomial = c(multinomial_probabilities,multinomial_categories)
+    )
+    
+    type_lengths <-
+      sapply(type_check,length)
+    
+    if (sum(type_lengths > 0) > 1) {
+      stop(
+        "Please enter parameters for only one kind of variable (linear, binary or multinomial)."
       )
+    }
+    
+    if (!is.null(linear_mean)) {
+      if (!is.null(linear_sd)) {
+        variable <- list(distribution = "normal",
+                         mean = linear_mean,
+                         sd = linear_sd)
+      }else{
+        variable <- list(distribution = "normal",
+                         mean = linear_mean,
+                         sd = linear_mean * .10 + 1)
       }
     }
-    if(!is.null(binary_probability)){
-      if(!is.null(binary_categories)){
+    
+    if (!is.null(binary_probability)) {
+      if (!is.null(binary_categories)) {
         variable <- list(
           distribution = "binary",
           probability = binary_probability,
@@ -43,12 +43,13 @@ declare_variable <- function(
         variable <- list(
           distribution = "binary",
           probability = binary_probability,
-          categories = c(0,1) 
+          categories = c(0,1)
         )
       }
     }
-    if(!is.null(multinomial_probabilities)){
-      if(!is.null(multinomial_categories)){
+    
+    if (!is.null(multinomial_probabilities)) {
+      if (!is.null(multinomial_categories)) {
         variable <- list(
           distribution = "multinomial",
           probability = multinomial_probabilities,
@@ -58,25 +59,23 @@ declare_variable <- function(
         variable <- list(
           distribution = "multinomial",
           probability = multinomial_probabilities,
-          categories = 1:length(multinomial_probabilities) 
+          categories = 1:length(multinomial_probabilities)
         )
       }
     }
-    if(!FALSE %in% unlist(lapply(
-      X = list(
-        normal = list(linear_mean,linear_sd),
-        binary = list(binary_probability,binary_categories),
-        multinomial = list(multinomial_probabilities,multinomial_categories)),
-      FUN = function(i)unlist(lapply(i,is.null)))) 
-    ){
-        variable <- list(
-          distribution = "normal",
-          mean = 0,
-          sd = 1
-        )    
-      }
+    
+    if (sum(type_lengths > 0) == 0) {
+      variable <- list(distribution = "normal",
+                       mean = 0,
+                       sd = 1)
+    }
+    
+    if (!is.null(transformation)) {
+      variable <- c(variable, transformation = transformation)
+    }
+    
     class(variable) <- "DGP_object"
+    
     return(variable)
+    
   }
-
-
