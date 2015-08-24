25988c29afbf90d4eb15c400420bb5cd1ee90bcf
macartan <macartan@gmail.com>
2015-08-24 05:03:17 -0400

    code for logo

---
+++ memos/logo.R
@@ -0,0 +1,7 @@
+
+frame()
+text(.425,.5, "experimentr", cex = 6, col = rgb(51/255,153/255,255/255), font=list(family="Courier"))
+points(.9,.575, cex = 5, col = rgb(51/255,153/255,255/255), lwd = 4)
+points(.9,.575, cex = 2, col = rgb(51/255,153/255,255/255), lwd = 5, pch = 16)
+points(.9,.425, cex = 5, col = rgb(51/255,153/255,255/255), lwd = 4, pch = 16)
+points(.9,.425, cex = 2, col = "white", lwd = 4, pch = 16)
