ba105064dc46748bd2378c43b82158ec05bc2baf
graemeblair <graemeblair@users.noreply.github.com>
2015-08-27 22:41:03 -0400

    logo up

--- memos/logo.R
+++ memos/logo.R
@@ -1,11 +1,8 @@
 
-dir <- tempdir()
-
-
 library(extrafont)
 loadfonts()
 
-pdf(file = paste0(dir, "/experimentr_logo_tmp.pdf"), width = 5.75, height = 4)
+pdf(file = "experimentr_logo_tmp.pdf", width = 5.75, height = 4)
 par(mar = rep(0, 4), omi = rep(0, 4))
 plot(0, 0, xlim = c(.03, 1), ylim = c(.1, .9), type = "n", axes = F, ylab = "", xlab = "")
 text(0.01, .48, "experimentr", pos = 4, cex = 5, col = rgb(51/255,153/255,255/255), family = "Open Sans")
@@ -18,10 +15,10 @@ points(.965,.46, cex = 3.2, col = rgb(51/255,153/255,255/255), lwd = 4)
 points(.965,.46, cex = 3.2, col = rgb(51/255,153/255,255/255), lwd = 4, pch = 16)
 points(.965,.46, cex = 2, col = "white", lwd = 4, pch = 16)
 dev.off()
-embed_fonts(paste0(dir, "/experimentr_logo_tmp.pdf"), outfile = paste0(dir, "/experimentr_logo.pdf"))
-system(paste("open", dir))
+embed_fonts("experimentr_logo_tmp.pdf", outfile = "experimentr_logo.pdf")
+system("rm experimentr_logo_tmp.pdf")
 
-svg(file = paste0(dir, "/experimentr_logo.svg"), width = 5.75, height = 4)
+svg(file = "experimentr_logo.svg", width = 5.75, height = 4)
 par(mar = rep(0, 4), omi = rep(0, 4))
 plot(0, 0, xlim = c(.03, 1), ylim = c(.1, .9), type = "n", axes = F, ylab = "", xlab = "")
 text(0.01, .48, "experimentr", pos = 4, cex = 5, col = rgb(51/255,153/255,255/255), family = "Open Sans")
