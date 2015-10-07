6582b8bcf2f52879110d0d00c6147f07d3930bd6
jaspercooper <jaspercooper@gmail.com>
2015-10-06 21:57:29 -0400

    Implemented expressions + strings as arguments to estimand and subset

--- R/declare_estimand.R
+++ R/declare_estimand.R
@@ -2,6 +2,16 @@
 declare_estimand <- function(estimand = NULL, target = "population", subset = NULL, weights_variable = NULL, 
                              custom_estimand_function = NULL, label = NULL, ...) {
   
+  
+  estimand <- quote(estimand)
+  
+  subset <- quote(subset)
+  
+  if(is.character(eval(subset))){
+    subset <- parse(text = subset)
+  }
+  
+  
   if(!is.null(custom_estimand_function) & !is.null(estimand)){
     stop("Please provide either an estimand as a string or an expression, or a custom_estimand_function.")
   }
@@ -10,17 +20,16 @@ declare_estimand <- function(estimand = NULL, target = "population", subset = NU
     
     ## if no custom estimand is provided
     
-    description <- as.character(estimand)
-    
-    if(!is.character(eval(estimand))){
-      estimand <- quote(estimand)
+    if(!is.character(estimand)){
+      description <- deparse(estimand)
     } else {
+      description <- estimand
       estimand <- parse(text = estimand)
     }
     
     estimand_function <- function(data){
       if(!is.null(subset))
-        data <- subset(data, subset = eval(parse(text = subset)))
+        data <- subset(data, subset = eval(subset))
       ##if(!is.null(weights_variable))
       ##  estimator_options$weights <- data[, weights_variable]
       return(eval(estimand, envir = data))
@@ -36,7 +45,7 @@ declare_estimand <- function(estimand = NULL, target = "population", subset = NU
     estimand_function <- function(data){
       argument_names <- names(formals(custom_estimand_function))
       if(!is.null(subset) & "subset" %in% argument_names)
-        estimand_options$subset <- with(data, eval(parse(text = subset)))
+        estimand_options$subset <- with(data, eval(subset))
       if(!is.null(weights_variable) & "weights" %in% argument_names)
         estimand_options$weights <- data[, weights_variable]
       estimand_options$data <- data
