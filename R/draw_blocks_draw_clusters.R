93f460483000df7c6a875769e1e11f5b1f949ef3
Graeme Blair <graemeblair@users.noreply.github.com>
2015-11-04 10:39:15 -0500

    added draw_clusters and draw_blocks functions

---
+++ R/draw_blocks_draw_clusters.R
@@ -0,0 +1,33 @@
+#' Draw blocks for custom block functions used in an assignment function
+#'
+#' @param data 
+#' @param assignment 
+#' @param random_seed 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+draw_blocks <- function(data, assignment, random_seed = NULL){
+  if(!is.null(assignment$custom_blocking_function)) { 
+    data[, assignment$block_variable_name] <- assignment$custom_blocking_function(data = data)
+  }
+  return(data)
+}
+
+#' Draw clusters for custom cluster functions used in an assignment function
+#'
+#' @param data 
+#' @param assignment 
+#' @param random_seed 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+draw_clusters <- function(data, assignment, random_seed = NULL){
+  if(!is.null(assignment$custom_clustering_function)){
+    data[, assignment$cluster_variable_name] <- assignment$custom_clustering_function(data = data)
+  }
+  return(data)
+}
