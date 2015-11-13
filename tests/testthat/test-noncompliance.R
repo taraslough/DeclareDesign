4793e81289741adffba40fd5fc427e643879d1b1
Alexander Coppock <acoppock@gmail.com>
2015-11-13 14:01:12 -0500

    noncompliance test

--- tests/testthat/test-noncompliance.R
+++ tests/testthat/test-noncompliance.R
@@ -6,25 +6,30 @@ context("Noncompliance")
 
 test_that("test whether noncompliance works", {
   
-  population <- declare_population(noise = declare_variable(), N = 1000)
+  population <- declare_population(noise = "rnorm(n_)", size = 1000)
   sampling <- declare_sampling(n = 500)
-  potential_outcomes <- declare_potential_outcomes(formula = Y ~ 5 + .5*Z + noise,
+  potential_outcomes <- declare_potential_outcomes(formula = Y ~ 5 + .5*D + noise,
                                                    condition_names = c(0, 1),
-                                                   assignment_variable_name = "Z")
+                                                   assignment_variable_name = "D")
   noncompliance <- declare_noncompliance(condition_names = c(0,1), 
                                          assignment_variable_name = "Z", 
-                                         compliance_proportions=c(1, .5), 
-                                         baseline_condition=0)
+                                         options = list(compliance_proportions=c(1, .5), 
+                                                        baseline_condition=0))
+                                          
   assignment <- declare_assignment(condition_names = c(0,1))
   
   
   # mock data  ---------------------------------------------------------------  
   
-  pop_draw <- draw_population(population = population)
+  pop_draw <- draw_population(population = population,
+                              potential_outcomes = list(potential_outcomes))
+  
+  
   smp_draw <- draw_sample(data = pop_draw, sampling = sampling)
   smp_draw <- assign_treatment(data = smp_draw, assignment = assignment)
   smp_draw <- draw_outcome(data = smp_draw, 
                            potential_outcomes = list(potential_outcomes, noncompliance))
+  head(pop_draw)
   head(smp_draw)
   with(smp_draw, table(Z, D))
   
