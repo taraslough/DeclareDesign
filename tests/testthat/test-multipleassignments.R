6b00a53e2c02611f7c37fe6f1793c8976f146426
Alexander Coppock <acoppock@gmail.com>
2015-12-14 16:22:19 -0500

    block on a prior treatment!

--- tests/testthat/test-multipleassignments.R
+++ tests/testthat/test-multipleassignments.R
@@ -12,6 +12,7 @@ test_that("test multiple assignments work", {
                                                    condition_names = list(Z1 = c(0, 1), 
                                                                           Z2 = c(0, 1)),
                                                    assignment_variable_name = c("Z1", "Z2"))
+  
   assignment_1 <- declare_assignment(condition_names = c(0, 1),assignment_variable_name  = "Z1")
   assignment_2 <- declare_assignment(condition_names = c(0, 1),assignment_variable_name  = "Z2")
   
@@ -27,3 +28,29 @@ test_that("test multiple assignments work", {
   
 
 })
+
+test_that("test multiple assignments work", {
+  
+  population <- declare_population(noise = "rnorm(n_)", size = 5000)
+  sampling <- declare_sampling(n = 4999)
+  potential_outcomes <- declare_potential_outcomes(formula = Y ~ 5 + 1*Z1 + 2*Z2 - 3*Z1*Z2 + noise,
+                                                   condition_names = list(Z1 = c(0, 1), 
+                                                                          Z2 = c(0, 1)),
+                                                   assignment_variable_name = c("Z1", "Z2"))
+  
+  assignment_1 <- declare_assignment(condition_names = c(0, 1),assignment_variable_name  = "Z1")
+  assignment_2 <- declare_assignment(condition_names = c(0, 1),assignment_variable_name  = "Z2", block_variable_name = "Z1")
+  
+  pop_draw <- draw_population(population = population, potential_outcomes = potential_outcomes)
+  smp_draw <- draw_sample(data = pop_draw, sampling = sampling)
+  
+  smp_draw <- assign_treatment(data = smp_draw, assignment = list(assignment_1, assignment_2))
+  smp_draw <- draw_outcome(data = smp_draw, potential_outcomes = potential_outcomes)
+  
+  with(smp_draw, table(Z1, Z2))
+  
+  summary(lm(Y~ Z1*Z2, data= smp_draw))
+  
+  
+})
+
