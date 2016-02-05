de1a41f874aeb8723105d72bd5532533a7ad0726
Graeme Blair <graemeblair@users.noreply.github.com>
2016-02-05 11:02:17 -0500

    added error if the argument the user provides is not needed

--- R/declare_design_from_template.R
+++ R/declare_design_from_template.R
@@ -11,14 +11,16 @@ declare_design_from_template <- function(template, ...){
     for (i in 1:length(options)) {
       if (names(options)[[i]] %in% argument_names) {
         options[[names(options)[[i]]]] <- options[[i]]
+      } else {
+        stop(paste("The argument", names(options)[[i]], "is not used by this template. Please remove it."))
       }
     }
   }
   
   design <- do.call(template, args = options)
   
-  if(class(design) != "design"){
-    stop("The template function you chose did not create a 'design' object. Please choose another template function")
+  if (class(design) != "design") {
+    stop("The template function you chose did not create a design object. Please choose another template function.")
   }
 
   return(design)
