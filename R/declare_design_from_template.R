c8ec4a00fa9ed333577a03bc7ad4b9030df8791c
Graeme Blair <graemeblair@users.noreply.github.com>
2016-02-05 10:44:32 -0500

    added declare_design_from_template

---
+++ R/declare_design_from_template.R
@@ -0,0 +1,26 @@
+#' @export
+declare_design_from_template <- function(template, ...){
+  
+  if (missing(template)) {
+    stop("You must specify a template function in the template argument.")
+  }
+  
+  argument_names <- names(formals(template))
+  options <- list(...)
+  if (length(options) > 0) {
+    for (i in 1:length(options)) {
+      if (names(options)[[i]] %in% argument_names) {
+        options[[names(options)[[i]]]] <- options[[i]]
+      }
+    }
+  }
+  
+  design <- do.call(template, args = options)
+  
+  if(class(design) != "design"){
+    stop("The template function you chose did not create a 'design' object. Please choose another template function")
+  }
+
+  return(design)
+  
+}
