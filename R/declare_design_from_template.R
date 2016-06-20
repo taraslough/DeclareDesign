0c932ccee2d0be26d07632a72c4530614870292c
jaspercooper <jaspercooper@gmail.com>
2016-06-19 20:32:41 -0400

    Created a function for warning user about what quick_design is up to

--- R/declare_design_from_template.R
+++ R/declare_design_from_template.R
@@ -96,3 +96,62 @@ get_arg_list <- function(constants,variables,variable_index){
   return(c(constants,variable_args))
 }
 
+
+set_intersection_warn_user <- function(intersection, variable_lengths){
+  
+  same_length <- length(unique(variable_lengths)) == 1
+  
+  if(is.null(intersection)){ # User has not set intersection
+    
+    if(same_length){ # All variables are of same length
+      
+      intersection <- FALSE
+      
+      N_designs <- unique(variable_lengths)
+      
+      user_message <- 
+        paste0("All arguments passed through vary(...) are of same length, so quick_design() will set intersection to FALSE by default and create ", N_designs, " unique design(s). \nIf you would like one design per unique combination of arguments passed through vary(...), set intersection = TRUE.")
+      
+      
+    } else{ # Variables are of different lengths 
+      
+      intersection <- TRUE
+      
+      N_designs <- prod(variable_lengths)
+      
+      user_message <- 
+        paste0("Arguments passed to quick_design() through vary(...) are of different lengths, so quick_design() will set intersection to TRUE and create ", N_designs, " unique design(s).")
+      
+    }
+  } else { # User has set intersection 
+    
+    if(!intersection & !same_length) { # User has set intersection = FALSE but the variables are of differing lengths
+      
+      user_message <- paste0(
+        "The arguments you have passed to quick_design() have the following lengths: ",paste(names(variable_lengths), "=",variable_lengths,collapse = ", "),", but you have set intersection = FALSE. Please ensure that either all variables have the same length or intersection = TRUE. Note that if you set intersection = TRUE, quick_design() will return one design for each unique combination of the variables (", prod(variable_lengths)," designs).")
+      
+      stop(user_message)
+      
+    }
+    if(intersection){ # User has set intersection = TRUE and vars are unequal or equal
+      
+      N_designs <- prod(variable_lengths)
+      
+      user_message <- 
+        paste0("You have set intersection = TRUE, so quick_design() will create ", N_designs, " design(s) out of the unique combinations of the arguments passed to it through vary(...).")
+      
+    } 
+    if(!intersection & same_length){ # User has set intersection = FALSE and vars are equal
+      
+      N_designs <- unique(variable_lengths)
+      
+      user_message <- 
+        paste0("You have set intersection = FALSE, so quick_design() will create ", N_designs, " design(s) as it iterates through the arguments passed to it through vary(...). Use intersection = TRUE to make one design for each unique combination of the variables (", prod(variable_lengths)," designs).")
+    }
+  }
+  message(user_message)
+  return(intersection)
+}
+
+
+
